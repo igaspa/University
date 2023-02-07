72505c6fd016764eac5737e31276922a538884a7
Ivana Gasparov <gasparovivana@outlook.com>
2023-02-07 15:33:29 +0100

    Fix database port for stage environment

--- server/src/database/config/config.js
+++ server/src/database/config/config.js
@@ -14,7 +14,7 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: 5434,
+    port: 5432,
     host: process.env.DB_HOST,
     dialect: 'postgres'
   },
