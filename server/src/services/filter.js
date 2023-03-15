71ca897143eca293a5c4dbba1b2ee30ca42cda07
Ivana <gasparovivana@outlook.com>
2023-03-15 14:49:16 +0100

    Add filters by course,department and major

--- server/src/services/filter.js
+++ server/src/services/filter.js
@@ -2,6 +2,9 @@ const { Sequelize } = require('../database/models');
 
 exports.searchByUserName = (queryParams) => {
   const option = [];
+  const additionalFilters = filters(queryParams);
+  option.push(...additionalFilters);
+
   if (queryParams.firstName) {
     const term = { first_name: { [Sequelize.Op.iLike]: '%' + queryParams.firstName + '%' } };
     option.push(term);
@@ -17,6 +20,23 @@ exports.searchByName = (queryParams) => {
   const option = [];
   if (queryParams.name) {
     const term = { name: { [Sequelize.Op.iLike]: '%' + queryParams.name + '%' } };
+    option.push(term);
+  }
+  return option;
+};
+
+const filters = (queryParams) => {
+  const option = [];
+  if (queryParams.course) {
+    const term = { course_id: queryParams.course };
+    option.push(term);
+  }
+  if (queryParams.department) {
+    const term = { department_id: queryParams.department };
+    option.push(term);
+  }
+  if (queryParams.major) {
+    const term = { major_id: queryParams.major };
     option.push(term);
   }
   return option;
