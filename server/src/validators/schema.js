c097ec7c8009e20ac11c54cb2df8f7aa09ba81c4
Ivana <gasparovivana@outlook.com>
2023-03-17 09:55:11 +0100

    Change schemas to support UUID instead of number id

--- server/src/validators/schema.js
+++ server/src/validators/schema.js
@@ -2,69 +2,75 @@ const Joi = require('joi');
 const { ValidationError } = require('../validators/customErrors');
 
 exports.idSchema = Joi.object({
-  id: Joi.number().min(1).max(100).required()
+  id: Joi.string().min(1).max(100).required()
 });
 
 exports.compositeIdSchema = Joi.object({
-  firstId: Joi.number().min(1).max(100).required(),
-  secondId: Joi.number().min(1).max(100).required()
+  firstId: Joi.string().min(1).max(100).required(),
+  secondId: Joi.string().min(1).max(100).required()
 });
 
 exports.courseSchema = Joi.object({
+  id: Joi.string().min(1).max(100).required(),
   name: Joi.string().required(),
   creditHours: Joi.number().min(0).max(5).required(),
-  majorId: Joi.number().min(1).required()
+  majorId: Joi.string().min(1).required()
 });
 
 exports.majorSchema = Joi.object({
+  id: Joi.string().min(1).max(100).required(),
   name: Joi.string().required()
 });
 
 exports.departmentSchema = Joi.object({
+  id: Joi.string().min(1).max(100).required(),
   name: Joi.string().required(),
-  majorId: Joi.number().min(1).required()
+  majorId: Joi.string().min(1).required()
 });
 
 exports.examSchema = Joi.object({
+  id: Joi.string().min(1).max(100).required(),
   name: Joi.string().required(),
   date: Joi.date().greater(Date.now() + 48 * 60 * 60 * 1000),
-  courseId: Joi.number().min(1).required()
+  courseId: Joi.string().min(1).required()
 });
 
 exports.enrollmentSchema = Joi.object({
-  studentId: Joi.number().required().min(1),
-  courseId: Joi.number().required().min(1)
+  studentId: Joi.string().required().min(1),
+  courseId: Joi.string().required().min(1)
 });
 
 exports.lecturerSchema = Joi.object({
-  professorId: Joi.number().min(1).required(),
-  courseId: Joi.number().min(1).required()
+  professorId: Joi.string().min(1).required(),
+  courseId: Joi.string().min(1).required()
 });
 
 exports.resultSchema = Joi.object({
   grade: Joi.number().min(1).max(5).required(),
-  studentId: Joi.number().min(1).required(),
-  examId: Joi.number().min(1).required()
+  studentId: Joi.string().min(1).required(),
+  examId: Joi.string().min(1).required()
 });
 
 exports.studentSchema = Joi.object({
+  id: Joi.string().min(1).max(100).required(),
   firstName: Joi.string().required(),
   lastName: Joi.string().required(),
   email: Joi.string().min(3).max(50).required(),
   password: Joi.string().min(3).max(50).required(),
   address: Joi.string().min(3).max(50).required(),
   phoneNumber: Joi.string().max(15).required(),
-  majorId: Joi.number().min(1).required()
+  majorId: Joi.string().min(1).required()
 });
 
 exports.professorSchema = Joi.object({
+  id: Joi.string().min(1).max(100).required(),
   firstName: Joi.string().required(),
   lastName: Joi.string().required(),
   email: Joi.string().min(3).max(50).required(),
   password: Joi.string().min(3).max(50).required(),
   address: Joi.string().min(3).max(50).required(),
   phoneNumber: Joi.string().min(9).max(15).required(),
-  departmentId: Joi.number().min(1).required()
+  departmentId: Joi.string().min(1).required()
 });
 
 exports.authenticationSchema = Joi.object({
